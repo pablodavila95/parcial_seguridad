1227235c9bc9dd8b23314b298dd3fb0e3ac516bd
Pablo Davila <a00513157@itesm.mx>
2020-04-28 16:55:19 -0500

    some cleaning alv

--- neuralnet/permutation.py
+++ neuralnet/permutation.py
@@ -6,7 +6,7 @@ def decrypt(plaintext, key):
     x = 0
     k = len(key)
     ntimes = int(len(plaintext)/k)
-    for w in range(0, ntimes):
+    for _ in range(0, ntimes):
         for i in range(0, k):
             lp = int(key[i]) + (k*x) - 1
             ciphertext.append(plaintext[lp])
@@ -18,17 +18,18 @@ def encrypt(plaintext, key):
     plaintext = list(plaintext)
     key = list(key.strip())
     ciphertext = []
+    x = 0
+    k = len(key)
 
     addL = True
     while addL:
-        if len(plaintext) % len(key) == 0:
+        if len(plaintext) % k == 0:
             addL = False
         else:
             plaintext.append('-')
-    x = 0
-    k = len(key)
+
     ntimes = int(len(plaintext)/k)
-    for w in range(0, ntimes):
+    for _ in range(0, ntimes):
         for i in range(0, k):
             lp = int(key[i]) + (k*x) - 1
             ciphertext.append(plaintext[lp])
